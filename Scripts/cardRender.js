d07c83e26a44debe0606416a72020cd176179c13
prokn4h <meetproknah@gmail.com>
2016-08-15 14:17:28 +0500

    Показывает руку игрока

--- Scripts/cardRender.js
+++ Scripts/cardRender.js
@@ -2,6 +2,6 @@
 cardRender = function(){ 
 	for (n=0; n < playerHand.length; n++) {
 		imgSource = "images/" + playerHand[n].name + playerHand[n].color + ".png";	
-		document.write("<img id=\"playerCard\" src=imgSource onerror=\"this.src='images/not_found.png'\">");
+		document.write("<img id = \"playerCard\" src = imgSource onerror = \"this.src = 'images/not_found.png'\">");
 	}
 };
