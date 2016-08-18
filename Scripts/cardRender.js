ddc4ed2ed0df21573884cb3af928f47078513161
prokn4h <meetproknah@gmail.com>
2016-08-18 14:28:05 +0500

    Update cardRender.js

--- Scripts/cardRender.js
+++ Scripts/cardRender.js
@@ -1,20 +1,15 @@
-// Показывает все карты в руке игрока или дилера
-cardRender = function(dealer){ 
-	switch(dealer) {
-		case false:
-			$("#playerHand").empty();
-			for (n=0; n < playerHand.length; n++) {
-				imgSource = "images/" + playerHand[n].name + playerHand[n].color + ".png";
-					$("#playerHand").append("<img id=\"playerCard\" src=imgSource onerror=\"this.src='images/not_found.png'\">");
-			}
-			break;
-		case true:
-			$("#dealerHand").empty();
-			for (n=0; n < dealerHand.length-1; n++) {
-				imgSource = "images/" + dealerHand[n].name + dealerHand[n].color + ".png";
-					$("#dealerHand").append("<img id=\"dealerCard\" src=imgSource onerror=\"this.src='images/not_found.png'\">");
-			}
-			$("#dealerHand").append("<img id=\"dealerCard\" src='images/back.png' onerror=\"this.src='images/not_found.png'\">");
-			break;			
-	};
-};
+// Показывает руки игрока и дилера (0 - показать все карты, 1 - скрыть последнюю карту дилера)
+cardRender = function(hide) { 
+	$(".hand").empty();
+	for (var n = 0; n < playerHand.length; n++) {
+		imgSource = "images/" + playerHand[n].name + playerHand[n].color + ".png";
+			$("#playerHand").append("<img id=\"playerCard\" src=imgSource onerror=\"this.src='images/not_found.png'\">");
+	}
+	for (n = 0; n < dealerHand.length - hide; n++) {
+		imgSource = "images/" + dealerHand[n].name + dealerHand[n].color + ".png";
+			$("#dealerHand").append("<img id=\"dealerCard\" src=imgSource onerror=\"this.src='images/not_found.png'\">");
+	}
+	if (hide == 1) {
+		$("#dealerHand").append("<img id=\"dealerCard\" src='images/back.png' onerror=\"this.src='images/not_found.png'\">");		
+	}
+}
