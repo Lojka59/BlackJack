b4f69077e8f1e02d5cd019b19b239f0bcbe0bcff
prokn4h <meetproknah@gmail.com>
2016-08-18 12:25:06 +0500

    Update cardRender.js

--- Scripts/cardRender.js
+++ Scripts/cardRender.js
@@ -1,7 +1,20 @@
-// Показывает все карты в руке игрока (если не может показать картинку - показывает плейсхолдер)
-cardRender = function(){ 
-	for (n=0; n < playerHand.length; n++) {
-		imgSource = "images/" + playerHand[n].name + playerHand[n].color + ".png";	
-		document.write("<img id = \"playerCard\" src = imgSource onerror = \"this.src = 'images/not_found.png'\">");
-	}
+// Показывает все карты в руке игрока или дилера
+cardRender = function(dealer){ 
+	switch(dealer) {
+		case false:
+			$("#playerHand").empty();
+			for (n=0; n < playerHand.length; n++) {
+				imgSource = "images/" + playerHand[n].name + playerHand[n].color + ".png";
+					$("#playerHand").append("<img id=\"playerCard\" src=imgSource onerror=\"this.src='images/not_found.png'\">");
+			}
+			break;
+		case true:
+			$("#dealerHand").empty();
+			for (n=0; n < dealerHand.length-1; n++) {
+				imgSource = "images/" + dealerHand[n].name + dealerHand[n].color + ".png";
+					$("#dealerHand").append("<img id=\"dealerCard\" src=imgSource onerror=\"this.src='images/not_found.png'\">");
+			}
+			$("#dealerHand").append("<img id=\"dealerCard\" src='images/back.png' onerror=\"this.src='images/not_found.png'\">");
+			break;			
+	};
 };
